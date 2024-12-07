541b4ef0d18a22b7e7eb16a9a46ebd20bd71821c
Arthur Axiotes <axiotesarthur@gmail.com>
2024-12-07 15:02:50 -0300

    feat: estrutura da pagina de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Eye, EyeOff, LucideAngularModule } from 'lucide-angular';
+import { LucideIconData } from 'lucide-angular/icons/types';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [LucideAngularModule, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  public typeInput: string = 'password';
+  public eyePassword: LucideIconData = EyeOff;
 
+  public changeTypeInput(): void {
+    if (this.typeInput === 'password') {
+      this.typeInput = 'text';
+      this.eyePassword = Eye;
+
+      return;
+    }
+
+    this.typeInput = 'password';
+    this.eyePassword = EyeOff;
+  }
 }
