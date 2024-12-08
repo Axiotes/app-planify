ffe51d8f5f3c536c695148213e0e1914a53c959b
Arthur Axiotes <axiotesarthur@gmail.com>
2024-12-07 22:58:33 -0300

    feat: definicao de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,28 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { AgendaComponent } from './pages/agenda/agenda.component';
+import { ActivityComponent } from './pages/activity/activity.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  {
+    path: 'agenda',
+    component: AgendaComponent,
+    children: [
+      {
+        path: 'new-activity',
+        component: ActivityComponent,
+        data: { type: 'new' },
+      },
+      {
+        path: 'edit-activity/:id',
+        component: ActivityComponent,
+        data: { type: 'edit' },
+      },
+    ],
+  },
   { path: '', pathMatch: 'full', redirectTo: '/login' },
   { path: '**', redirectTo: '/login' },
 ];
