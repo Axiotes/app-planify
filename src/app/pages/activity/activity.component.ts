a5c18f0525f0be192931f386d39013ae5f7be252
Arthur Axiotes <axiotesarthur@gmail.com>
2024-12-08 22:43:37 -0300

    feat: navegacao entre as telas

--- src/app/pages/activity/activity.component.ts
+++ src/app/pages/activity/activity.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ArrowLeft, LucideAngularModule, User } from 'lucide-angular';
 import { LucideIconData } from 'lucide-angular/icons/types';
 
 @Component({
   selector: 'app-activity',
   standalone: true,
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, RouterLink],
   templateUrl: './activity.component.html',
   styleUrl: './activity.component.scss',
 })
