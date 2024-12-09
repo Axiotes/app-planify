aa293027299801bcb147f03df3fa9068db204160
Arthur Axiotes <axiotesarthur@gmail.com>
2024-12-08 23:09:51 -0300

    feat: pegar os valores do form

--- src/app/pages/activity/activity.component.ts
+++ src/app/pages/activity/activity.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { ArrowLeft, LucideAngularModule, User } from 'lucide-angular';
 import { LucideIconData } from 'lucide-angular/icons/types';
@@ -6,11 +12,24 @@ import { LucideIconData } from 'lucide-angular/icons/types';
 @Component({
   selector: 'app-activity',
   standalone: true,
-  imports: [LucideAngularModule, RouterLink],
+  imports: [LucideAngularModule, RouterLink, ReactiveFormsModule],
   templateUrl: './activity.component.html',
   styleUrl: './activity.component.scss',
 })
 export class ActivityComponent {
   public back: LucideIconData = ArrowLeft;
   public user: LucideIconData = User;
+
+  public formActivity: FormGroup = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    date: new FormControl('', [Validators.required]),
+    time: new FormControl('', [Validators.required]),
+    description: new FormControl(''),
+    priority: new FormControl(''),
+    alert: new FormControl(''),
+  });
+
+  public save(): void {
+    console.log(this.formActivity.value);
+  }
 }
