65d1f49937728694a2e70b61a80153e5cb018420
Arthur Axiotes <axiotesarthur@gmail.com>
2024-12-10 13:11:34 -0300

    feat: estilizacao da pagina

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { ArrowLeft, LucideAngularModule, User } from 'lucide-angular';
+import { LucideIconData } from 'lucide-angular/icons/types';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
 })
 export class UserComponent {
-
+  public user: LucideIconData = User;
+  public back: LucideIconData = ArrowLeft;
 }
