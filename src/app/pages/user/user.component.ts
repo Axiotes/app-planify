519bcdaffbc7c0ee37707991a185703a60800f8e
Arthur Axiotes <axiotesarthur@gmail.com>
2024-12-10 23:55:16 -0300

    feat: pegar informacoes do usuario do backend

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ArrowLeft, LucideAngularModule, User } from 'lucide-angular';
 import { LucideIconData } from 'lucide-angular/icons/types';
+import { ApiRequestsService } from '../../services/api-requests.service';
+import { Router } from '@angular/router';
+import { AlertComponent } from '../../components/alert/alert.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-user',
@@ -9,7 +13,33 @@ import { LucideIconData } from 'lucide-angular/icons/types';
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
   public user: LucideIconData = User;
   public back: LucideIconData = ArrowLeft;
+
+  public email: string = '';
+
+  constructor(
+    private apiRequestsService: ApiRequestsService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {
+    this.apiRequestsService.userInfo().subscribe({
+      next: (res) => {
+        this.email = res.email;
+      },
+      error: (err) => {
+        this.snackBar.openFromComponent(AlertComponent, {
+          duration: 5000,
+          data: {
+            message: err.error.message,
+          },
+        });
+
+        this.router.navigateByUrl('/agenda');
+      },
+    });
+  }
 }
