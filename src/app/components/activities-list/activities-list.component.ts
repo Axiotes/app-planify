c13f461108d8d999ef5a70d26fe1689b0df6eb46
Arthur Axiotes <axiotesarthur@gmail.com>
2024-12-13 12:21:51 -0300

    feat: alterar tamanho da lista de atividades

--- src/app/components/activities-list/activities-list.component.ts
+++ src/app/components/activities-list/activities-list.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LucideAngularModule, Minus } from 'lucide-angular';
+import { LucideIconData } from 'lucide-angular/icons/types';
+import { Activity } from '../../../types/activity.type';
+import { ActivitiesComponent } from '../activities/activities.component';
 
 @Component({
   selector: 'app-activities-list',
   standalone: true,
-  imports: [],
+  imports: [LucideAngularModule, ActivitiesComponent],
   templateUrl: './activities-list.component.html',
-  styleUrl: './activities-list.component.scss'
+  styleUrl: './activities-list.component.scss',
 })
 export class ActivitiesListComponent {
+  @Output() public changeHeight: EventEmitter<number> =
+    new EventEmitter<number>();
+  @Input() public activities!: Activity[];
 
+  public minus: LucideIconData = Minus;
+
+  private touchMoved: boolean = false;
+  private startX: number = 0;
+  private startY: number = 0;
+
+  public touchStart(event: TouchEvent): void {
+    this.touchMoved = false;
+    this.startX = event.touches[0].clientX;
+    this.startY = event.touches[0].clientY;
+  }
+
+  public touchMove(event: TouchEvent): void {
+    if (!this.touchMoved) {
+      this.touchMoved = true;
+
+      const currentX = event.touches[0].clientX;
+      const currentY = event.touches[0].clientY;
+      const deltaX = currentX - this.startX;
+      const deltaY = currentY - this.startY;
+
+      if (Math.abs(deltaX) < Math.abs(deltaY)) {
+        if (deltaX > 0) {
+          // Baixo
+          this.changeHeight.emit(40);
+        } else {
+          // Cima
+          this.changeHeight.emit(100);
+        }
+      }
+    }
+  }
 }
--- src/app/pages/agenda/agenda.component.ts
+++ src/app/pages/agenda/agenda.component.ts
@@ -2,15 +2,23 @@ import { Component } from '@angular/core';
 import { CalendarComponent } from '../../components/calendar/calendar.component';
 import { LucideAngularModule, User } from 'lucide-angular';
 import { LucideIconData } from 'lucide-angular/icons/types';
-import { ActivitiesListComponent } from "../../components/activities-list/activities-list.component";
+import { ActivitiesListComponent } from '../../components/activities-list/activities-list.component';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-agenda',
   standalone: true,
-  imports: [CalendarComponent, LucideAngularModule, ActivitiesListComponent],
+  imports: [
+    CalendarComponent,
+    LucideAngularModule,
+    ActivitiesListComponent,
+    NgStyle,
+  ],
   templateUrl: './agenda.component.html',
   styleUrl: './agenda.component.scss',
 })
 export class AgendaComponent {
   public user: LucideIconData = User;
+
+  public height: number = 40;
 }
