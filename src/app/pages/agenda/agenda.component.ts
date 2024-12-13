6a371b423064a5ab10192252e19994244a1e5dd4
Arthur Axiotes <axiotesarthur@gmail.com>
2024-12-13 11:33:18 -0300

    wip: estrutura da tela de agenda

--- src/app/pages/agenda/agenda.component.ts
+++ src/app/pages/agenda/agenda.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { CalendarComponent } from "../../components/calendar/calendar.component";
+import { CalendarComponent } from '../../components/calendar/calendar.component';
+import { LucideAngularModule, User } from 'lucide-angular';
+import { LucideIconData } from 'lucide-angular/icons/types';
+import { ActivitiesListComponent } from "../../components/activities-list/activities-list.component";
 
 @Component({
   selector: 'app-agenda',
   standalone: true,
-  imports: [CalendarComponent],
+  imports: [CalendarComponent, LucideAngularModule, ActivitiesListComponent],
   templateUrl: './agenda.component.html',
-  styleUrl: './agenda.component.scss'
+  styleUrl: './agenda.component.scss',
 })
 export class AgendaComponent {
-
+  public user: LucideIconData = User;
 }
