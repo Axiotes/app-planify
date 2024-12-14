1d25a085d8f64abd8565b0e6124b28a3e704914e
Arthur Axiotes <axiotesarthur@gmail.com>
2024-12-13 21:10:45 -0300

    feat: estrutura do componente de atividades

--- src/app/components/activities/activities.component.ts
+++ src/app/components/activities/activities.component.ts
@@ -1,12 +1,16 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { LucideAngularModule, Star } from 'lucide-angular';
+import { LucideIconData } from 'lucide-angular/icons/types';
 
 @Component({
   selector: 'app-activities',
   standalone: true,
-  imports: [],
+  imports: [LucideAngularModule, NgFor],
   templateUrl: './activities.component.html',
-  styleUrl: './activities.component.scss'
+  styleUrl: './activities.component.scss',
 })
 export class ActivitiesComponent {
-
+  public priority: number = 3;
+  public stars: LucideIconData[] = [Star, Star, Star];
 }
