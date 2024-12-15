884e3505e0ffb1fe97939b6f6e650f9373375584
Arthur Axiotes <axiotesarthur@gmail.com>
2024-12-15 20:21:07 -0300

    fix: exibir se tarefa ja foi feita

--- src/app/components/activities/activities.component.ts
+++ src/app/components/activities/activities.component.ts
@@ -1,15 +1,16 @@
 import { NgFor } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { LucideAngularModule, Star } from 'lucide-angular';
 import { LucideIconData } from 'lucide-angular/icons/types';
 import { TimePipe } from '../../pipes/time.pipe';
 import { TrucatePipe } from '../../pipes/trucate.pipe';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-activities',
   standalone: true,
-  imports: [LucideAngularModule, NgFor, TimePipe, TrucatePipe, RouterLink],
+  imports: [LucideAngularModule, NgFor, TimePipe, TrucatePipe, RouterLink, FormsModule],
   templateUrl: './activities.component.html',
   styleUrl: './activities.component.scss',
 })
